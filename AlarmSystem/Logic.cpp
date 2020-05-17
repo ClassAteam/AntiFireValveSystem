1956200844fd87320ffbbc9a50ea2be38645d952
Yuri Khramov <royal_88@mail.ru>
2020-05-17 09:58:33 +0300

    Max typing

--- AlarmSystem/Logic.cpp
+++ AlarmSystem/Logic.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 #include "Header.h"
 void logic()
 {
@@ -9,46 +13,281 @@ void logic()
 			BSS811X1p = true;
 		}
 		else
-			BSS811X1p = false;	
-		if (K50 == true)
+			BSS811X1p = false;
+		if ((Ushal >= 18.0) || K50 || K51 == true)
 		{
-			if (K51 == true)
-			{
-				BSS811X1n = true;
-			}
-			else BSS811X1n = false;
+			BSS811X1n = true;
 		}
+		else BSS811X1n = false;
 	}
-	if ((Ushap >= 18) && (F25 == true))
+	if ((Ushap >= 18) || K50 || K51 == true)
+	{
+		PSA10_1 = true;
+	}
+	else
+		PSA10_1 = false;
+
+	if (Ushal >= 18 && F25 == true)
 	{
 		BSS811X1x = true;
 	}
-	else
-		BSS811X1x = false;
-
+	else BSS811X1x = false;
 	//////////////////////////////2nd engine check
 	if (Ushal >= 18.0)
 	{
-		if (K50 || K51 || LzhSrabSigPrg1dv == true)
+		if (K53 || K54 || LzhSrabSigPrg2dv == true)
 		{
-			BSS811X1p = true;
+			BSS811X1t = true;
 		}
 		else
-			BSS811X1p = false;
-		if (K50 == true)
+			BSS811X1t = false;
+		if ((Ushal >= 18.0) || K53 || K54 == true)
 		{
-			if (K51 == true)
-			{
-				BSS811X1n = true;
-			}
-			else BSS811X1n = false;
+			BSS811X1r = true;
 		}
+		else BSS811X1r = false;
 	}
-	if ((Ushap >= 18) && (F25 == true))
+	if ((Ushap >= 18) || K53 || K54 == true)
 	{
-		BSS811X1x = true;
+		PSA10_2 = true;
 	}
 	else
-		BSS811X1x = false;
-	
+		PSA10_2 = false;
+
+	if (Ushap >= 18 && F35 == true)
+	{
+		BSS811X1z = true;
+	}
+	else BSS811X1z = false;
+	//////////////////////////////3rd engine check
+	if (Ushap >= 18.0)
+	{
+		if (K57 || K58 || LzhSrabSigPrg3dv == true)
+		{
+			BSS812X5j = true;
+		}
+		else
+			BSS812X5j = false;
+
+		if ((Ushap >= 18.0) || K57 || K58 == true)
+		{
+			BSS812X5h = true;
+		}
+		else BSS812X5h = false;
+	}
+	if ((Ushap >= 18) || K57 || K58 == true)
+	{
+		PSA19_1 = true;
+	}
+	else
+		PSA19_1 = false;
+
+	if (Ushal >= 18 && F55 == true)
+	{
+		BSS913X3E = true;
+	}
+	else BSS913X3E = false;
+	//////////////////////////////4th engine check
+	if (Ushap >= 18.0)
+	{
+		if (K60 || K61 || LzhSrabSigPrg4dv == true)
+		{
+			BSS812X5p = true;
+		}
+		else
+			BSS812X5p = false;
+
+		if ((Ushap >= 18.0) || K60 || K61 == true)
+		{
+			BSS812X5n = true;
+		}
+		else BSS812X5n = false;
+	}
+	if ((Ushal >= 18) || K60 || K61 == true)
+	{
+		PSA19_2 = true;
+	}
+	else
+		PSA19_2 = false;
+
+	if (Ushal >= 18 && F65 == true)
+	{
+		BSS913X3G = true;
+	}
+	else BSS913X3G = false;
+	///////////////////////////////////BSS811X1B BSS838X7G toggle;
+	if (F72 == true && S10 == true)
+		PKO = true;
+	else PKO = false;
+
+	if (Ushal >= 18.0 && K24 == 1)
+	{
+		BSS811X1B = true;
+		BSS838X7G = true;
+	}
+	else
+	{
+		BSS811X1B = false;
+		BSS838X7G = false;
+	}
+
+	if ((Ushal >= 18) && F45 == true)
+	{
+		BSS811X1VV = true;
+		BSS838X7C = true;
+	}
+	else
+	{
+
+		BSS811X1VV = false;
+		BSS838X7C = false;
+	}
+	/////////////////////////////////////BSS913X3J toggle
+	if (F91 == true)
+	{
+		if (PW1och_o == false)
+		{
+			BSS913X3J = true;
+		}
+		else
+		{
+			if (PKO == true)
+			{
+				if (F101 == true)
+				{
+					BSS913X3J = true;
+				}
+			}
+			else
+			{
+				if (PO1och == true);
+				BSS913X3J = true;
+				BSS913X3J = false;
+			}
+		}
+	}BSS913X3J = false;
+	/////////////////////////////////////
+	/*if (F91 == true)
+	{
+		if (PW1och_L == false)            !!! Specification issue
+		{
+			BSS913X3J = true;
+		}
+		else
+		{
+			if (PKO == true)
+			{
+				if (F101 == true)
+				{
+					BSS913X3J = true;
+				}
+			}
+			else
+			{
+				if (PO1och == true);
+				BSS913X3J = true;
+				BSS913X3J = false;
+			}
+		}
+	}BSS913X3J = false;*/
+
+	//////////////////////////BSS913X3L and BSS926X1h toggle
+	if (F91 == true)
+	{
+		if (PW2och == false)
+		{
+			BSS913X3L = true;
+			BSS926X1h = true;
+		}
+		if (PKO == true)
+		{
+			if (PF11 - 1 == true)
+			{
+				BSS913X3L = true;
+				BSS926X1h = true;
+			}
+			else
+			{
+				BSS913X3L = false;
+				BSS926X1h = false;
+			}
+		}
+		else
+		{
+			BSS913X3L = false;
+			BSS926X1h = false;
+		}
+	}
+	else
+		BSS913X3L = false;
+	BSS926X1h = false;
+	///////////////////////////////////BSS913X3N and BSS926X1j toggle;
+	if ((PF9 - 1) == 1)
+	{
+		if (PW3och == false)
+		{
+			BSS913X3N = true;
+			BSS926X1j = true;
+		}
+		else
+		{
+			if (PKO == true)
+			{
+				if (F11 == true)
+				{
+					BSS913X3N = true;
+					BSS926X1j = true;
+				}
+				else
+				{
+					BSS913X3N = false;
+					BSS926X1j = false;
+				}
+			}
+			else
+			{
+				BSS913X3N = false;
+				BSS926X1j = false;
+			}
+		}
+	}
+	else
+	{
+		BSS913X3N = false;
+		BSS926X1j = false;
+	}
+	///////////////////////////////////// K80 toggle
+	if (F72 == true && P2OBLOP == true)
+	{
+		K80 = true;
+	}
+	else
+		K80 = false;
+	//////////////////////////////////// PO1och toggle
+	if (F91 == true && K80 == true)
+	{
+		if (F101 == F181 == true)
+		{
+			PO1och = false;
+		}
+		else
+			PO1och = true;
+	}
+	PO1och = false;
+	//////////////////////////////////// BSS811X1v toggle
+	if (F91 == true)
+	{
+		if (F132 == true)
+		{
+			BSS811X1v = false;
+		}
+		else
+		{
+			BSS811X1v = true;
+		}
+	}
+	else
+	{
+		BSS811X1v = false;
+	}
 }
--- AlarmSystem/Test.cpp
+++ AlarmSystem/Test.cpp
@@ -5,23 +5,38 @@
 #include <iomanip>
 #include <ostream>
 
-bool K15, K16, K19, K20, K23, K24, K25, K26, K27, K28, K50, K53, K51, K54, K57,
-K58, K60, K61, F25, F35, F45, F55, F65, F72, F82, F132, F142;
-
-double UbusL, UbusR, Uacc;
-
-int F7, F8, S3, S4, S5, S6, S7, S8, S9;
+ bool PKO, PO1och, K24, K50, K51, K53, K54, K57, K58, K60, K61, K80;
+ bool F11, F25, F35, F45, F55, F65, F72, F132, F91, F101, F111, F121, F181;
+ bool PW1och_o, PW2och, PW3och, P2OBLOP;
+ bool PSA10_1, PSA10_2, PSA19_1, PSA19_2;
+ double Ushal, Ushap;
+ int S10, PF11, PF9;
+ bool LzhSrabSigPrg1dv, LzhSrabSigPrg2dv, LzhSrabSigPrg3dv, LzhSrabSigPrg4dv;
+ bool BSS811X1n, BSS811X1r, BSS812X5h, BSS812X5n, BSS811X1x, BSS811X1z, BSS913X3E, BSS913X3G, BSS811X1B, BSS838X7G, BSS811X1VV, BSS838X7C, BSS913X3J, BSS926X1F
+, BSS913X3L, BSS926X1h, BSS913X3N, BSS926X1j, BSS811X1v, BSS811X1p, BSS811X1t, BSS812X5j, BSS812X5p;
 
 void logic();
 
-int printvalues()
-{
-	std::cout.setf(std::ios::right);
-	std::cout << std::setw(5) << K15 << std::setw(5) << K16 << std::setw(5) << K19 << std::setw(5) << K20 << std::setw(5) << K23 << std::setw(5) << K24 << std::setw(5) << K25 << std::setw(5) << K26 << std::setw(5) << K27 << std::setw(5) << K28 << std::setw(5) << K50 << std::setw(5) << K53 << std::setw(5) << K51 << std::setw(5) << K54 << std::setw(5) << K57 << std::setw(5) <<
-		K58 << std::setw(5) << K60 << std::setw(5) << K61 << std::setw(5) << F25 << std::setw(5) << F35 << std::setw(5) << F45 << std::setw(5) << F55 << std::setw(5) << F65 << std::setw(5) << F72 << std::setw(5) << F82 << std::setw(5) << F132 << std::setw(5) << F142 << std::setw(5) << UbusL << std::setw(5) << UbusR << std::setw(5) << Uacc
-		<< std::setw(5) << F7 << std::setw(5) << F8 << std::setw(5) << S3 << std::setw(5) << S4 << std::setw(5) << S5 << std::setw(5) << S6 << std::setw(5) << S7 << std::setw(5) << S8 << std::setw(5) << S9 << std::endl;
-	return 0;
-};
+//int printvalues()
+//{
+//	std::cout.setf(std::ios::right);
+//	std::cout << std::setw(20) << PKO << std::setw(20) << PO1och << std::setw(20) << K24 << std::setw(20) << K50 << std::setw(20) << K51 
+//		<< std::setw(20) << K53 << std::setw(20) << K54 << std::setw(20) << K57 << std::setw(20) << K58 << std::setw(20) << K60 
+//		<< std::setw(20) << K61 << std::setw(20) << K80 << std::setw(20) << F11 << std::setw(20) << F25 << std::setw(20) << F35 
+//		<< std::setw(20) <<F45 << std::setw(20) << F55 << std::setw(20) << F65 << std::setw(20) << F72 << std::setw(20) << F132 << std::setw(20) 
+//		<< F91 << std::setw(20) << F101 << std::setw(20) << F111 << std::setw(20) << F121 << std::setw(20) << F181 
+//		<< std::setw(20) << PW1och_o << std::setw(20) << PW2och << std::setw(20) << PW3och << std::setw(20) << P2OBLOP << std::setw(20) << PSA10_1
+//		<< std::setw(20) << PSA10_2 << std::setw(20) << PSA19_1 << std::setw(20) << PSA19_2 << std::setw(20) << Ushal << std::setw(20) << Ushap 
+//		<< std::setw(20) << S10 << std::setw(20) << PF11 << std::setw(20) << PF9 << std::setw(20) << LzhSrabSigPrg1dv << std::setw(20)
+//		<< LzhSrabSigPrg2dv << std::setw(20) << LzhSrabSigPrg3dv << std::setw(20) << LzhSrabSigPrg4dv << std::setw(20) << BSS811X1n << std::setw(20) 
+//		<< BSS811X1r << std::setw(20)<< BSS812X5h << std::setw(20) << BSS812X5n << std::setw(20) << BSS811X1x << std::setw(20) << BSS811X1z << std::setw(20) 
+//		<< BSS913X3E << std::setw(20)
+//		<< BSS913X3G << std::setw(20) << BSS811X1B << std::setw(20) << BSS838X7G << std::setw(20) << BSS811X1VV << std::setw(20) << BSS838X7C << std::setw(20)
+//		<< BSS913X3J << std::setw(20) << BSS926X1F << std::setw(20) << BSS913X3L << std::setw(20) << BSS926X1h 
+//		<< std::setw(20) << BSS913X3N << std::setw(20) << BSS926X1j << std::setw(20) << BSS811X1v << std::setw(20)
+//		<< BSS811X1p << std::setw(20) << BSS811X1t << std::setw(20) << BSS812X5j << std::setw(20) << BSS812X5p << std::setw(20)<<std::endl;
+//	return 0;
+//};
 
 int main()
 {
@@ -33,246 +48,406 @@ int main()
 
 	while (std::getline(file, word))
 	{
-		if (word == "K15")
+		if (word == "PKO")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			K15 = stoi(word);
+			PKO = stoi(word);
+			std::cout << std::endl;
+			std::cout << word << std::endl;
 
 		}
-		if (word == "K16")
+		if (word == "PO1och")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			K16 = stoi(word);
-		}
-		if (word == "K19")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K19 = stoi(word);
-		}
-		if (word == "K20")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K20 = stoi(word);
-		}
-		if (word == "K23")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K23 = stoi(word);
+			PO1och = stoi(word);
 		}
 		if (word == "K24")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K24 = stoi(word);
 		}
-		if (word == "K25")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K25 = stoi(word);
-		}
-		if (word == "K26")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K26 = stoi(word);
-		}
-		if (word == "K27")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K27 = stoi(word);
-		}
-		if (word == "K28")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			K28 = stoi(word);
-		}
 		if (word == "K50")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K50 = stoi(word);
 		}
 		if (word == "K51")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K51 = stoi(word);
 		}
 		if (word == "K53")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K53 = stoi(word);
 		}
 		if (word == "K54")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K54 = stoi(word);
 		}
 		if (word == "K57")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K57 = stoi(word);
 		}
 		if (word == "K58")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K58 = stoi(word);
 		}
 		if (word == "K60")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K60 = stoi(word);
 		}
 		if (word == "K61")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			K61 = stoi(word);
 		}
+		if (word == "K80")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			K80 = stoi(word);
+		}
+		if (word == "F11")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			F11 = stoi(word);
+		}
 		if (word == "F25")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F25 = stoi(word);
 		}
 		if (word == "F35")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F35 = stoi(word);
 		}
 		if (word == "F45")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F45 = stoi(word);
 		}
 		if (word == "F55")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F55 = stoi(word);
 		}
 		if (word == "F65")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F65 = stoi(word);
 		}
 		if (word == "F72")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F72 = stoi(word);
 		}
-		if (word == "F82")
-		{
-			std::cout << std::setw(5) << std::right << word;
-			std::getline(file, word);
-			F82 = stoi(word);
-		}
 		if (word == "F132")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
 			F132 = stoi(word);
 		}
-		if (word == "F142")
+		if (word == "F91")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			F142 = stoi(word);
+			F91 = stoi(word);
 		}
-		if (word == "UbusL")
+		if (word == "F101")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			UbusL = stod(word);
+			F101 = stoi(word);
 		}
-		if (word == "UbusR")
+		if (word == "F111")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			UbusR = stod(word);
+			F111 = stoi(word);
 		}
-		if (word == "Uacc")
+		if (word == "F121")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			Uacc = stod(word);
+			F121 = stoi(word);
 		}
-		if (word == "F7")
+		if (word == "F181")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			F7 = stoi(word);
+			F181 = stoi(word);
 		}
-		if (word == "F8")
+		if (word == "PW1och_o")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			F8 = stoi(word);
+			PW1och_o = stoi(word);
 		}
-		if (word == "S3")
+		if (word == "PW2och")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S3 = stoi(word);
+			PW2och = stoi(word);
 		}
-		if (word == "S4")
+		if (word == "PW3och")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S4 = stoi(word);
+			PW3och = stoi(word);
 		}
-		if (word == "S5")
+		if (word == "P2OBLOP")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S5 = stoi(word);
+			P2OBLOP = stoi(word);
 		}
-		if (word == "S6")
+		if (word == "PSA10_1")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S6 = stoi(word);
+			PSA10_1 = stoi(word);
 		}
-		if (word == "S7")
+		if (word == "PSA10_2")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S7 = stoi(word);
+			PSA10_2= stoi(word);
 		}
-		if (word == "S8")
+		if (word == "PSA19_1")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S8 = stoi(word);
+			PSA19_1 = stoi(word);
 		}
-		if (word == "S9")
+		if (word == "PSA19_2")
 		{
-			std::cout << std::setw(5) << std::right << word;
+			std::cout << std::setw(20) << std::right << word;
 			std::getline(file, word);
-			S9 = stoi(word);
+			PSA19_2 = stoi(word);
 		}
+		if (word == "Ushal")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			Ushal = stod(word);
+		}
+		if (word == "Ushap")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			Ushap= stod(word);
+		}
+		if (word == "S10")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			S10 = stoi(word);
+		}
+		if (word == "PF11")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			PF11 = stoi(word);
+		}
+		if (word == "PF9")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			PF9 = stoi(word);
+		}
+		if (word == "LzhSrabSigPrg1dv")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			LzhSrabSigPrg1dv = stoi(word);
+		}
+		if (word == "LzhSrabSigPrg2dv")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			LzhSrabSigPrg2dv = stoi(word);
+		}
+		if (word == "LzhSrabSigPrg3dv")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			LzhSrabSigPrg3dv = stoi(word);
+		}
+		if (word == "LzhSrabSigPrg4dv")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			LzhSrabSigPrg4dv = stoi(word);
+		}
+		if (word == "BSS811X1n")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1n = stoi(word);
+		}
+		if (word == "BSS811X1r")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1r = stoi(word);
+		}
+		if (word == "BSS812X5h")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS812X5h = stoi(word);
+		}
+		if (word == "BSS812X5n")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS812X5n = stoi(word);
+		}
+
+		if (word == "BSS811X1x")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1x= stoi(word);
+		}
+		if (word == "BSS811X1z")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1z= stoi(word);
+		}
+		if (word == "BSS913X3E")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS913X3E= stoi(word);
+		}
+		if (word == "BSS913X3G")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS913X3G = stoi(word);
+		}
+		if (word == "BSS811X1B")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1B = stoi(word);
+		}
+		if (word == "BSS838X7G")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS838X7G= stoi(word);
+		}
+		if (word == "BSS811X1VV")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1VV = stoi(word);
+		}
+		if (word == "BSS838X7C")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS838X7C = stoi(word);
+		}
+		if (word == "BSS913X3J")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS913X3J = stoi(word);
+		}
+		if (word == "BSS926X1F")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS926X1F = stoi(word);
+		}
+		if (word == "BSS913X3L")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS913X3L = stoi(word);
+		}
+		if (word == "BSS926X1h")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS926X1h= stoi(word);
+		}
+		if (word == "BSS913X3N")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS913X3N = stoi(word);
+		}
+		if (word == "BSS926X1j")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS926X1j = stoi(word);
+		}
+		if (word == "BSS811X1v")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1v = stoi(word);
+		}
+		if (word == "BSS811X1p")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1p = stoi(word);
+		}
+		if (word == "BSS811X1t")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS811X1t = stoi(word);
+		}
+		if (word == "BSS812X5j")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS812X5j = stoi(word);
+		}
+		if (word == "BSS812X5p")
+		{
+			std::cout << std::setw(20) << std::right << word;
+			std::getline(file, word);
+			BSS812X5p = stoi(word);
+		}
+		
 	}
 	std::cout << std::endl;
-	printvalues();
+	/*printvalues();*/
 	logic();
-	printvalues();
+	/*printvalues();*/
 }
 /*std::vector<std::string> lines;
 	std::ifstream file("input.txt");
