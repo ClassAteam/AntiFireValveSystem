3e1059d0e1667963a817cd7b8bcc68741340af5d
Nahuy Idi <royal_88@mail.ru>
2020-05-18 13:50:37 +0300

    Executive block logic init

--- ExecutionBlock/Header.h
+++ ExecutionBlock/Header.h
@@ -3,4 +3,4 @@ extern bool PPBI2, K1, K3, K6, K9, K12, K24, K31, K32, K33, K34, K35, K42, K49, 
 F25, F35, F45, F55, F65, F82, F92, F91, F101, F111, F121, F181, F911, F912, F913, F914, F915, F916, F917, F72, F82, P2OBLOP, PPBI, K2;
 extern double Ush1dpl, Ush1dpp, Ush2dpl, Ush2dpp;
 extern int AZSF9, AZSF10, AZSF18, AZSF11, AZSF12, S1, S2, S11, S13, S14, S15, PNU, PW1och_l, PW1och_o, PW2och, PW3och, OtkazSPZ,
-pozhar_1dv, pozhar_2dv, pozhar_3dv, pozhar_4dv, peregrev_1dv, peregrev_2dv, peregrev_3dv, peregrev_4dv, pozhar_vsu, F135, F145, F155, F165;
+pozhar_1dv, pozhar_2dv, pozhar_3dv, pozhar_4dv, peregrev_1dv, peregrev_2dv, peregrev_3dv, peregrev_4dv, pozhar_vsu, F135, F145, F155, F165, failure_automatic;
--- ExecutionBlock/Logic.cpp
+++ ExecutionBlock/Logic.cpp
@@ -130,7 +130,7 @@ void logic()
 	{
 		K12 = false;
 	}
-//						F911 toggle
+	//						F911 toggle
 	if (F92 == true && S2 == 1)
 	{
 		F911 = true;
@@ -269,9 +269,577 @@ void logic()
 	{
 		K2 = false;
 	}
-	//						K49, K42, K67 toggle
-	if (F25 == true)
+	//						K42, K49toggle
+	if (F25 || F35 || F45 || F55 || F65 == true)
 	{
 		if (F121 == true)
+		{
+			if (S13 == 1)			//!!! non-defined switch, mb need enum type
+			{
+				K49 = true;
+			}
+			else
+			{
+				if (S14 == 1)
+				{
+					K49 = true;
+				}
+				else
+				{
+					K49 = false;
+				}
+			}
+		}
+		else
+		{
+			K49 = false;
+		}
+	}
+	else
+	{
+		K49 = false;
+		K42 = false;
+	}
+	//					K67 toggle
+	if (F101 == true)
+	{
+		if (S15 == 1)			//!!! the same here
+		{
+			K67 = true;
+		}
+		else
+		{
+			K67 = false;
+		}
+	}
+	else
+	{
+		K67 = false;
+	}
+	//							K31toggle
+	if (F25 == true)
+	{
+		if (K67 != true)
+		{
+			if (F135 == true)
+			{
+				if (failure_automatic == true)
+				{
+					K31 = false;
+				}
+				else
+				{
+					K31 = true;
+				}
+			}
+			else
+			{
+				K31 = false;
+			}
+		}
+		else
+		{
+			K31 = true;
+		}
+	}
+	else
+	{
+		K31 = false;
+	}
+	//							K32toggle
+	if (F35 == true)
+	{
+		if (K67 != true)
+		{
+			if (F145 == true)
+			{
+				if (failure_automatic == true)
+				{
+					K32 = false;
+				}
+				else
+				{
+					K32 = true;
+				}
+			}
+			else
+			{
+				K32 = false;
+			}
+		}
+		else
+		{
+			K32 = true;
+		}
+	}
+	else
+	{
+		K32 = false;
+	}
+	//							K33toggle
+	if (F45 == true)
+	{
+		if (K67 != true)
+		{
+				if (failure_automatic == true)
+				{
+					K33 = false;
+				}
+				else
+				{
+					K33 = true;
+				}
+		}
+		else
+		{
+			K33 = true;
+		}
+	}
+	else
+	{
+		K33 = false;
+	}
+	//							K34toggle
+	if (F55 == true)
+	{
+		if (K67 != true)
+		{
+			if (F155 == true)
+			{
+				if (failure_automatic == true)
+				{
+					K34 = false;
+				}
+				else
+				{
+					K34 = true;
+				}
+			}
+			else
+			{
+				K34 = false;
+			}
+		}
+		else
+		{
+			K34 = true;
+		}
+	}
+	else
+	{
+		K34 = false;
+	}
+	//							K35toggle
+	if (F65 == true)
+	{
+		if (K67 != true)
+		{
+			if (F165 == true)
+			{
+				if (failure_automatic == true)
+				{
+					K35 = false;
+				}
+				else
+				{
+					K35 = true;
+				}
+			}
+			else
+			{
+				K35 = false;
+			}
+		}
+		else
+		{
+			K35 = true;
+		}
+	}
+	else
+	{
+		K35 = false;
+	}
+	//							K50 toggle
+	if (F82 == true)
+	{
+		if (K3 == true)
+		{
+			if (F911 == true)
+			{
+				K50 = true;
+			}
+			else
+			{
+				if (F912 == true)
+				{
+					K50 = true;
+				}
+				else
+				{
+					if (F917 == true)
+					{
+						K50 = true;
+					}
+					else
+					{
+						if (pozhar_1dv == true)
+						{
+							K50 = true;
+						}
+						else
+						{
+							K50 = false;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if (pozhar_1dv == true)
+			{
+				K50 = true;
+			}
+			else
+			{
+				K50 = false;
+			}
+		}
+	}
+	else
+	{
+		K50 = false;
+	}
+	//							K51 toggle
+	if (F72 != true)
+	{
+		K51 = false;
+	}
+	else
+	{
+		if (K3 == true)
+		{
+			if (pozhar_1dv == true)
+			{
+				K51 = true;
+			}
+			else
+			{
+				if (peregrev_1dv == true)
+				{
+					K51 = true;
+				}
+				else
+				{
+					K51 = false;
+				}
+			}
+		}
+		else
+		{
+			if (F914 || F915 || F916 || F917 ||  pozhar_1dv || peregrev_1dv)
+			{
+				K51 = true;
+			}
+			else
+			{
+				K51 = false;
+			}
+		}
+	}
+	//							K53 toggle
+	if (F82 == true)
+	{
+		if (K6 == true)
+		{
+			if (F911 == true)
+			{
+				K53 = true;
+			}
+			else
+			{
+				if (F912 == true)
+				{
+					K53 = true;
+				}
+				else
+				{
+					if (F917 == true)
+					{
+						K53 = true;
+					}
+					else
+					{
+						if (pozhar_2dv == true)
+						{
+							K53 = true;
+						}
+						else
+						{
+							K53 = false;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if (pozhar_1dv == true)
+			{
+				K50 = true;
+			}
+			else
+			{
+				K50 = false;
+			}
+		}
+	}
+	else
+	{
+		K50 = false;
+	}
+	//							K54 toggle
+	if (F72 != true)
+	{
+		K54 = false;
+	}
+	else
+	{
+		if (K6 == true)
+		{
+			if (pozhar_2dv == true)
+			{
+				K54 = true;
+			}
+			else
+			{
+				if (peregrev_2dv == true)
+				{
+					K54 = true;
+				}
+				else
+				{
+					K54 = false;
+				}
+			}
+		}
+		else
+		{
+			if (F914 || F915 || F916 || F917 || pozhar_2dv || peregrev_2dv)
+			{
+				K54 = true;
+			}
+			else
+			{
+				K54 = false;
+			}
+		}
+	}
+	//							K57 toggle
+	if (F72 == true)
+	{
+		if (K9 == true)
+		{
+			if (F911 == true)
+			{
+				K57 = true;
+			}
+			else
+			{
+				if (F912 == true)
+				{
+					K57 = true;
+				}
+				else
+				{
+					if (F917 == true)
+					{
+						K57 = true;
+					}
+					else
+					{
+						if (pozhar_3dv == true)
+						{
+							K57 = true;
+						}
+						else
+						{
+							K57 = false;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if (pozhar_3dv == true)
+			{
+				K57 = true;
+			}
+			else
+			{
+				K57 = false;
+			}
+		}
+	}
+	else
+	{
+		K50 = false;
+	}
+	//							K58 toggle
+	if (F72 != true)
+	{
+		K58 = false;
+	}
+	else
+	{
+		if (K6 == true)
+		{
+			if (pozhar_2dv == true)
+			{
+				K58 = true;
+			}
+			else
+			{
+				if (peregrev_2dv == true)
+				{
+					K58 = true;
+				}
+				else
+				{
+					K58 = false;
+				}
+			}
+		}
+		else
+		{
+			if (F914 || F915 || F916 || F917 || pozhar_3dv || peregrev_3dv)
+			{
+				K58 = true;
+			}
+			else
+			{
+				K58 = false;
+			}
+		}
+	}
+	//							K60 toggle
+	if (F72 == true)
+	{
+		if (K12 == true)
+		{
+			if (F911 == true)
+			{
+				K60 = true;
+			}
+			else
+			{
+				if (F912 == true)
+				{
+					K60 = true;
+				}
+				else
+				{
+					if (F917 == true)
+					{
+						K60 = true;
+					}
+					else
+					{
+						if (pozhar_4dv == true)
+						{
+							K60 = true;
+						}
+						else
+						{
+							K60 = false;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if (pozhar_4dv == true)
+			{
+				K60 = true;
+			}
+			else
+			{
+				K60 = false;
+			}
+		}
+	}
+	else
+	{
+		K60 = false;
+	}
+	//							K61 toggle
+	if (F82 != true)
+	{
+		K61 = false;
+	}
+	else
+	{
+		if (K6 == true)
+		{
+			if (pozhar_4dv == true)
+			{
+				K61 = true;
+			}
+			else
+			{
+				if (peregrev_4dv == true)
+				{
+					K61 = true;
+				}
+				else
+				{
+					K61 = false;
+				}
+			}
+		}
+		else
+		{
+			if (F914 || F915 || F916 || F917 || pozhar_4dv || peregrev_4dv)
+			{
+				K61 = true;
+			}
+			else
+			{
+				K61 = false;
+			}
+		}
+	}
+	//					PW toggles
+	if (PNU = true)
+	{
+		PW1och_l = true;
+		PW1och_o = true;
+		PW2och = true;
+		PW3och = true;
+	}
+
+	if (K31  || K32 || K34 || K35 || K33 == true)
+	{
+			if (F101 == true)
+			{
+				PW1och_l = false;
+				if (F181 == true)
+				{
+					PW1och_o = false;
+				}
+			}
+	}
+	if (F111 == true && K42 == true)
+	{
+		PW2och = false;
+	}
+	else
+	{
+		if (F121 == true && K49 == true)
+		{
+			PW3och = false;
+		}
 	}
 }
