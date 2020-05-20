b82885859483e7fd20de91f88b1e91acb9f36ba8
Nahuy Idi <royal_88@mail.ru>
2020-05-20 08:48:22 +0300

    3rd Hydrosystem algorithm init within header

---
+++ 3rdHydroSystemResolvingAlgorithm/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+extern bool PTNUGS1, PTNUGS2, PTNUGS3, PTNUGS4, PNN1gs3, PNN2gs3, PNTNUgs3, KKGS3;
+
+extern double Pv_vsu, Pgs1, Pgs2, Pgs3, Pgs4, P0gs3, Pgat, Pgs3_r, Pgat_r, Qtnu_gs1, Qtnu_gs2, Qtnu_gs3, Qtnu_gs4, Qntnu_gs1, Qntnu_gs2, Qntnu_gs3, Qntnu_gs4, Qngs1, Qngs2, Qngs3, Qngs4,
+Qngn1gs1, Qngn1gs2, Qngn1gs3, Qngn1gs4, Qngn2gs1, Qngn2gs2, Qngn2gs3, Qngn2gs4, Qutgs3, Qn_gat, Qpst, Qpgs3, Qpz_sum, Wpgs3, Wpgs3_delta, d_Xst_dt, Kgs, Cst; 
+
+
