a8fddf50b4e4a34984e9601ff0d4c763f4bc3b34
Nahuy Idi <royal_88@mail.ru>
2020-05-28 15:09:16 +0300

    Preasure regulation complete

--- PresureRegulationAlgorithm/Header.h
+++ PresureRegulationAlgorithm/Header.h
@@ -1,9 +1,11 @@
 #pragma once
-extern bool K1_2131, K2_2131, K4_2131, PAVARR, PPVSKV, PPP, PGK, BSS838X5MM, BSS824X1G, BSS824X1J, BSS838X7A ;
+extern bool K1_2131, K2_2131, K4_2131, PAVARR, PPVSKV, PPP, PGK, BSS838X5MM, BSS824X1G, BSS824X1J, BSS838X7A, SKD_300, SKD_301, firstiteration,
+otkaz_razgermetizatsiya, lyukizagermetizirovany, otkaz_perenadduv;
 
-extern double Ush2dpl, H, Ph, Phmsa, Ph_msa, Ph_, Pkab, Pkab_delta, Pkab_ind_delta, Nkab, Nkab_ind, t1, t2, t3, t4;
+extern double Ush2dpl, H, Ph, Phmsa, Ph_msa, Ph_, Pkab, Pkab_delta, Pkab_ind_delta, Nkab, Nkab_ind, t1, t2, t3, t4, t_c,
+Vkab, Pkab_zad, Pkab_zad_buf; // tc is 0 untill it's resolved (time for iteration cycle). ????????if or global external ???????????
 
-extern int S1_2131, S2_2131, SKD_D300;
+extern int S1_2131, S2_2131;
 
 void logic();
 
--- PresureRegulationAlgorithm/Logic.cpp
+++ PresureRegulationAlgorithm/Logic.cpp
@@ -5,8 +5,11 @@
 #include "Header.h"
 #include <math.h>
 
+bool firstiteration = true;
 void logic()
 {
+	//			initialization values
+
 	PAVARR = false;
 	PPP = false;
 	PGK = false;
@@ -15,6 +18,7 @@ void logic()
 	K4_2131 = false;
 	Ph_ = Ph * 0, 00136;
 	Phmsa = presure_at_height(Ph_msa);
+	
 
 	if (Ush2dpl >= 18.0)
 	{
@@ -53,9 +57,228 @@ void logic()
 
 	if (K1_2131 == true)
 	{
-		SKD_D300 = 2;
+		BSS838X7A = true;
+		t1 = t1 + t_c;
+
+		if (t1 >= 0.192)
+		{
+			t2 = t2 + t_c;
+			BSS838X7A = false;
+			
+			if (t2 >= 0.192)
+			{
+				t1 = false;
+			}
+		}
+	}
+	else
+	{
+		t1 = t2 = false;
+		SKD_300 = false;
 	}
 
+	if (K2_2131 == true)
+	{
+		SKD_301 = true;
+		t3 = t3 + t_c;
+	}
+	else
+	{
+		SKD_301 = false;
+		BSS838X5MM = false;
+		BSS824X1G = false;
+		BSS824X1J = false;
+		t3 = t4 = 0.0;
+	}
+
+	if (t3 >= 0.192)
+	{
+		t4 = t4 + t_c;
+		BSS838X5MM = false;
+		BSS824X1G = false;
+		BSS824X1J = false;
+
+		if (t4 >= 0.192)
+		{
+			t3 = false;
+		}
+	}
+	else
+	{
+		BSS838X5MM = true;
+		BSS824X1G = true;
+		BSS824X1J = false;
+		t4 = false;
+	}
+	if (firstiteration)
+	{
+		Pkab = Ph_;
+		Nkab = 0.0;
+		Pkab_delta = 0.0;
+		Pkab_zad = Ph;
+		firstiteration = false;
+	}
+	else
+	{
+		if (otkaz_razgermetizatsiya)
+		{
+			Pkab_zad = Ph_;
+			Vkab = (0.0068 / 5);
+		}
+		else
+		{
+			if (PPVSKV)
+			{
+				if (lyukizagermetizirovany)
+				{
+					if (otkaz_perenadduv)
+					{
+						Pkab_zad = Phmsa + 0.8;
+						Vkab = (0.0068 / 5);
+					}
+					else
+					{
+						if (PAVARR)
+						{
+							Pkab_zad = Ph_;
+						}
+						else
+						{
+							if (PPP && H >= 7300)
+							{
+								Pkab_zad = Ph_msa + 0.36;
+							}
+							else
+							{
+								if (PGK)
+								{
+									if ((H >= -500) && (H <= 3700))
+									{
+										Pkab_zad = Phmsa + 0.12;
+									}
+									else
+									{
+										if ((H > 2400) && (H < 11500))
+										{
+											Pkab_zad = 0.77;
+										}
+										else
+										{
+											if ((H > 11500) && (H < 15000))
+											{
+												Pkab_zad = (0.77 + (0.33 * (11.5 - H)));
+											}
+											else
+											{
+												Pkab_zad = (Phmsa + 0.36);
+											}
+										}
+									}
+								}
+								else
+								{
+									if ((H >= -500) && (H <= 2400))
+									{
+										Pkab_zad = Ph_;
+									}
+									else
+									{
+										if ((H > 2400) && (H < 11500))
+										{
+											Pkab_zad = 0.77;
+										}
+										else
+										{
+											if ((H > 11500) && (H < 15000))
+											{
+												Pkab_zad = (0.77 + (0.33 * (11.5 - H)));
+											}
+											else
+											{
+												Pkab_zad = (Phmsa + 0.36);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					Pkab_zad = Ph_;
+					Vkab = (0.0068 / 5);
+				}
+			}
+			else
+			{
+				Pkab_zad = Ph_;
+				Vkab = (0.0068 / 5);
+			}
+		}
+	}
+	if (!firstiteration)
+	{
+		if (Pkab_zad >= Pkab_zad_buf)
+		{
+			Vkab = (0.0068 / 5);
+		}
+		else
+		{
+			Vkab = (0.0136 / 10);
+		}
+		Pkab_zad_buf = Pkab_zad;
+	}
+	if (!firstiteration)
+	{
+		if ((Pkab_zad - Pkab) >= 0.01)
+		{
+			if (Pkab < Pkab_zad)
+			{
+				Pkab = (Pkab + (Vkab / t_c));
+			}
+			else
+			{
+				Pkab = (Pkab - (Vkab / t_c));
+			}
+		}
+	}
+
+	Nkab == presure_at_height(Pkab);
+
+	Pkab_delta = Pkab - Ph_;
+
+	if (Nkab > 2000)
+	{
+		Nkab_ind = 2000;
+	}
+	else
+	{
+		if (Nkab < 0.0)
+		{
+			Nkab_ind = 0.0;
+		}
+		else
+		{
+			Nkab_ind = (Nkab / 1000);
+		}
+	}
+
+	if (Pkab_delta > 0.8)
+	{
+		Pkab_ind_delta = 0.8;
+	}
+	else
+	{
+		if (Pkab < (-0.06))
+		{
+			Pkab_ind_delta = (-0.06);
+		}
+		else
+		{
+			Pkab_ind_delta = Pkab_delta;
+		}
+	}
 
 
 
