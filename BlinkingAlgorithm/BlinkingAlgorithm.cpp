ebcc6b7b08526f140bdf9b5c6bf5d7a18b278bec
Nahuy Idi <royal_88@mail.ru>
2020-06-10 15:55:13 +0300

    Blinking algorithm init

---
+++ BlinkingAlgorithm/BlinkingAlgorithm.cpp
@@ -0,0 +1,48 @@
+#include  "Alarm2ndNaviHeader.h"
+#include  "AlarmNaviHeader.h"
+#include  "AlarmRightPilotHeader.h"
+
+int BlinkingLogic()
+{
+	BSS838X6k = BSS939X2A = BSS824X2R = BSS824X2f = 
+	BSS824X2h = BSS825X6E = BSS825X6Y = BSS825X6a =
+	BSS926X2U = false;
+
+
+	if (BSS838X7G)
+	{
+		BSS838X6k = true;
+	}
+
+	if (BSS939X1B)
+	{
+		BSS939X2A = true;
+	}
+
+	if (BSS824X1d)
+	{
+		BSS824X2R = true;
+	}
+
+	if(BSS824X1MM)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
