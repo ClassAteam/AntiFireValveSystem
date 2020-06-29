321ba5a0848d38a5aac3ae86fe83370595631f91
Nahuy Idi <royal_88@mail.ru>
2020-06-29 16:03:22 +0300

    landing gear sashes init and linear algorithm designed for resolve functions

---
+++ LandingGear_Sashes/LandingGear_Sashes.cpp
@@ -0,0 +1,7 @@
+#include "LandingGear_Sashes.h"
+#include "linear_algorithm.h"
+
+int LandingGear_sashes_logic()
+{
+	
+}
---
+++ LandingGear_Sashes/LandingGear_Sashes.h
@@ -0,0 +1,27 @@
+#pragma once
+
+int LandingGear_sashes_logic();
+
+bool
+GK_oovsh,
+GK_vsh,
+GK_ush,
+GK_avl,
+GK_avp,
+GK_avn;
+
+double
+delta_stv_l,
+delta_stv_p,
+delta_stv_n,
+delta_sh_l,
+delta_sh_p,
+delta_sh_n,
+Ddelta_stv,
+t_rv2l, t_rv2p, t_rv6l, t_rv6p, t_rv8n, 
+Pgs2,
+P_bal_l,
+P_bal_p,
+P_bal_per;
+
+
