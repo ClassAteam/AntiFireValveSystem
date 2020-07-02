62666b56f9b650f455aa6259104da2460227e310
Nahuy Idi <royal_88@mail.ru>
2020-07-02 15:18:06 +0300

    Rename header

--- LandingGerar_PrimalRacksCards/LandingGear_PrimalRacksCards.cpp
+++ LandingGerar_PrimalRacksCards/LandingGear_PrimalRacksCards.cpp
@@ -1,1 +1,111 @@
-#include ""
+#include "LandingGear_PrimalRacksCards.h"
+#include "linear_algorithm.h"
+
+int RacksCardsLogic()
+{
+	if (GK_oovsh == false)
+	{
+		if (Pgs2 >= 130.0)
+		{
+			Ddelta_tel = two_points_to_Y(Pgs2, 130.0, 280.0, 0.0, 0.25);
+		}
+		else
+		{
+			Ddelta_tel = false;
+		}
+
+		if (GK_vsh == false)
+		{
+			// left card moving
+			t_rv7l = 0.0;
+
+			if (GK_ush == true && fi_delta_sh_l == 1.0)
+			{
+				delta_tel_l = delta_tel_l - Ddelta_tel;
+			}
+			if (delta_tel_l <= 0.0)
+			{
+				delta_tel_l = 0.0;
+			}
+
+			// right card moving
+			t_rv7p = 0.0;
+
+			if (GK_ush == true && fi_delta_sh_p == 1.0)
+			{
+				delta_tel_p = delta_tel_p - Ddelta_tel;
+			}
+			if (delta_tel_p <= 0.0)
+			{
+				delta_tel_p = 0.0;
+			}
+		}
+		else
+		{
+			// if valve are closed
+			// left
+			if (delta_stv_l == 90.0)
+			{
+				t_rv7l = t_rv7l + tc;
+			}
+			else
+			{
+				t_rv7l = 0.0;
+			}
+
+			if (t_rv7l >= 0.5 && delta_sh_l >= 0.4)
+			{
+				delta_tel_l = delta_tel_l + Ddelta_tel;
+			}
+
+			if (delta_tel_l >= 1.0)
+			{
+				delta_tel_l = 1.0;
+			}
+
+			// right
+			if (delta_stv_p == 90.0)
+			{
+				t_rv7p = t_rv7p + tc;
+			}
+			else
+			{
+				t_rv7p = 0.0;
+			}
+
+			if (t_rv7p >= 0.5 && delta_sh_p >= 0.4)
+			{
+				delta_tel_p = delta_tel_p + Ddelta_tel;
+			}
+
+			if (delta_tel_p >= 1.0)
+			{
+				delta_tel_p = 1.0;
+			}
+		}
+	}
+	else
+	{
+		Ddelta_tel = a;
+
+		//left card
+		if (delta_sh_l >= 0.4)
+		{
+			delta_tel_l = delta_tel_l + Ddelta_tel;
+			if (delta_tel_l >= 1.0)
+			{
+				delta_tel_l = 1.0;
+			}
+		}
+
+		//right card
+		if (delta_sh_p >= 0.4)
+		{
+			delta_tel_p = delta_tel_p + Ddelta_tel;
+			if (delta_tel_p >= 1.0)
+			{
+				delta_tel_p = 1.0;
+			}
+		}
+	}
+}
--- LandingGerar_PrimalRacksCards/LandingGear_PrimalRacksCards.h
+++ LandingGerar_PrimalRacksCards/LandingGear_PrimalRacksCards.h
@@ -1,34 +1,31 @@
 #pragma once
 
-int racks_shifting_logic();
+int RacksCardsLogic();
 
+const double a = 0.16;
 bool
 GK_oovsh,
 GK_vsh, 
 GK_ush,
-GK_avl,
-GK_avp,
-GK_avn,
-GK_dvl,
-GK_dvp,
 otkaz_nedovipush_osn_opr;
 double
-delta_stv_l, 
-delta_stv_p, 
-delta_stv_n, 
-delta_sh_l, 
-delta_sh_p, 
-delta_sh_n, 
-delta_tel_l, 
-delta_tel_p, 
+delta_stv_l,
+delta_stv_p,
+delta_stv_n,
+delta_sh_l,
+delta_sh_p,
+delta_sh_n,
+delta_tel_l,
+delta_tel_p,
 
 fi_delta_sh_l,
 fi_delta_sh_p,
 Ddelta_stv,
+Ddelta_tel,
 Dfi_delta_sh_l,
 Dfi_delta_sh_p,
 Dfi_delta_sh,
-t_rv9l, t_rv9p, tc,
+t_rv7l, t_rv7p, tc,
 Pgs2, 
 Pbal_l, 
 Pbal_p, 
